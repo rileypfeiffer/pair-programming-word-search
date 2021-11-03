02d43f60f0ef5aabd3055f47f2aeb098ab200036
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-11-03 16:41:36 -0400

    Add vertical functionality and test cases for wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let verticalJoin = [];
+    if (letters.length !== 0 && letters[0].length !== undefined) {
+      for (let i = 0; i < letters[0].length; i++) {
+        verticalJoin.push([]);
+        for (let j = 0; j < letters.length; j++) {
+          verticalJoin[i].push([letters[j][i]]);
+        }
+      }
+      verticalJoin = verticalJoin.map(ls => ls.join(""));
+  
+      for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+          return true;
+        }
+      }
+      for (let l of verticalJoin) {
+        if (l.includes(word)) {
+          return true;
+        }
+      }
+    } return false;
+  };
 
-module.exports = wordSearch
+module.exports = wordSearch;
